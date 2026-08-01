da4d336fe839a73763ce63c8e923b14a3aba3986
Praneethha <160530815+PraneethhaRB@users.noreply.github.com>
2026-08-01 10:32:16 +0530

    Change API base URL to production server
    Updated base URL for API requests to the production server.

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:  "https://fitconnect-backend-jb9v.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
